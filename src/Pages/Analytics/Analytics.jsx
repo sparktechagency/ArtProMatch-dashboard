3fa5d9c5712edd64d27f87779af002352c181a6c
RabeyaAkter78 <rabeyariya251@gmail.com>
2025-03-27 10:40:40 +0600

    implement stats

--- src/Pages/Analytics/Analytics.jsx
+++ src/Pages/Analytics/Analytics.jsx
@@ -1,9 +1,15 @@
 import AnalyticsCards from "../../Components/PageComponents/AnalyticsCards";
+import AppointmentSummery from "../../Components/PageComponents/AppointmentSummery";
+import RevinueStats from "../../Components/PageComponents/RevinueStats";
 
 const Analytics = () => {
   return (
     <div>
       <AnalyticsCards />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <AppointmentSummery />
+        <RevinueStats />
+      </div>
     </div>
   );
 };
