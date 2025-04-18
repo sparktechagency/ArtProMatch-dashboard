fc5a0a599636e4c7eb440e903f2e607e70fe7e2e
Rabeya Akter <rabeyariya251@gmail.com>
2025-04-18 11:56:04 +0600

    conflict

--- src/Pages/Analytics/Analytics.jsx
+++ src/Pages/Analytics/Analytics.jsx
@@ -1,8 +1,8 @@
-import AllUser from "../../Components/PageComponents/AllUser";
-import AnalyticsCards from "../../Components/PageComponents/AnalyticsCards";
-import AppointmentSummery from "../../Components/PageComponents/AppointmentSummery";
-import RevinueStats from "../../Components/PageComponents/RevinueStats";
-import TopArtist from "../../Components/PageComponents/TopArtist";
+// import AllUser from "../../Components/PageComponents/AllUser";
+// import AnalyticsCards from "../../Components/PageComponents/AnalyticsCards";
+// import AppointmentSummery from "../../Components/PageComponents/AppointmentSummery";
+// import RevinueStats from "../../Components/PageComponents/RevinueStats";
+// import TopArtist from "../../Components/PageComponents/TopArtist";
 
 const Analytics = () => {
     return (
